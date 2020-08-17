cd0b5bd43fc2c6b9d19fa084533953c2825238eb
elmetacross <54289101+elmetacross@users.noreply.github.com>
2020-08-16 21:33:03 -0500

    Add files via upload

--- conexion.py
+++ conexion.py
@@ -1,13 +1,14 @@
 import mysql.connector as prueba
 
-mariadb_conexion = prueba.connect(host='localhost', port='3306',
+prueba = prueba.connect(host='localhost', port='3306',
                                   user='root', password='abc123', database='miprueba')
 
-cursor = mariadb_conexion.cursor()
+cursor = prueba.cursor()
+
 try:
     cursor.execute("SELECT ID,NOMBRE FROM Usuarios")
 
 except prueba.error as error:
     print("Error: {}".format(error))
-mariadb_conexion.close()
+prueba.close()
 
